2f9aa8c4b9c54388025beaced063e5d42cacd929
Eduardo Gautier <87209380+EduardoGautier@users.noreply.github.com>
2021-10-27 13:05:28 -0300

    Update Menu.java

--- src/com/company/projagd/model/Menu.java
+++ src/com/company/projagd/model/Menu.java
@@ -1,6 +1,5 @@
 package com.company.projagd.view.model;
-
-public class Menu<T extends Comparable<T>> {
+public class Menu<T> {
 
     private T objetoMenu;
 
@@ -8,9 +7,31 @@ public class Menu<T extends Comparable<T>> {
         this.objetoMenu = objetoMenu;
     }
 
+
+
+    /*public void exeMenu(){
+
+        System.out.println("┌----------------------------------------------------------┐\n" +
+                "│                       SISTEMA DE AGENDA                  │\n" +
+                "│----------------------------------------------------------│\n" +
+                "│ 1 - CADASTRAR CONTATO                                    │\n" +
+                "│ 2 - MOSTRAR TODOS OS CONTATOS                            │\n" +
+                "│ 3 - MOSTRAR QUANTIDADE DE CONTATOS                       │\n" +
+                "│ 4 - PESQUISAR CONTATO POR MÊS                            │\n" +
+                "│ 5 - PESQUISAR CONTATO POR CPF                            │\n" +
+                "│ 6 - PESQUISAR CONTATO POR NOME                           │\n" +
+                "│ 7 - ALTERAR TELEFONE                                     │\n" +
+                "│ 8 - REMOVER CONTATO  POR NOME                            │\n" +
+                "│ 9 - EXCLUIR TODOS OS CONTATOS                            │\n" +
+                "│ 0 - SAIR                                                 │\n" +
+                "└----------------------------------------------------------┘\n" +
+                "Digite a opção desejada:  ");
+
+    }*/
+
     @Override
     public String toString() {
-        return" ┌----------------------------------------------------------┐\n" +
+        return " ┌----------------------------------------------------------┐\n" +
                 " │                       SISTEMA DE AGENDA                  │ \n" +
                 " │----------------------------------------------------------│\n" +
                 " │ 1 - CADASTRAR CONTATO                                    │\n" +
@@ -27,3 +48,4 @@ public class Menu<T extends Comparable<T>> {
                 " Digite a opção desejada:  " + objetoMenu;
     }
 }
+
