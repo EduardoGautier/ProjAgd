09819b83df554ff961cd369698a7157f5318e981
Eduardo Gautier <87209380+EduardoGautier@users.noreply.github.com>
2021-10-27 08:11:13 -0300

    Create Menu.java

---
+++ src/com/company/projagd/model/Menu.java
@@ -0,0 +1,29 @@
+package com.company.projagd.view.model;
+
+public class Menu<T extends Comparable<T>> {
+
+    private T objetoMenu;
+
+    public Menu(T objetoMenu) {
+        this.objetoMenu = objetoMenu;
+    }
+
+    @Override
+    public String toString() {
+        return" ┌----------------------------------------------------------┐\n" +
+                " │                       SISTEMA DE AGENDA                  │ \n" +
+                " │----------------------------------------------------------│\n" +
+                " │ 1 - CADASTRAR CONTATO                                    │\n" +
+                " │ 2 - MOSTRAR TODOS OS CONTATOS                            │\n" +
+                " │ 3 - MOSTRAR QUANTIDADE DE CONTATOS                       │\n" +
+                " │ 4 - PESQUISAR CONTATO POR MÊS                            │\n" +
+                " │ 5 - PESQUISAR CONTATO POR CPF                            │\n" +
+                " │ 6 - PESQUISAR CONTATO POR NOME                           │\n" +
+                " │ 7 - ALTERAR TELEFONE                                     │\n" +
+                " │ 8 - REMOVER CONTATO  POR NOME                            │\n" +
+                " │ 9 - EXCLUIR TODOS OS CONTATOS                            │\n" +
+                " │ 0 - SAIR                                                 │\n" +
+                " └----------------------------------------------------------┘\n" +
+                " Digite a opção desejada:  " + objetoMenu;
+    }
+}
